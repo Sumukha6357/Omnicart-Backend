34c2d6882935a76fd6f70eb505f817ffb924085d
Sumukha K Y <107631405+Sumukha6357@users.noreply.github.com>
2025-07-26 14:55:28 +0530

    Update CorsConfig.java

--- src/main/java/com/sumuka/ecommerce_backend/config/CorsConfig.java
+++ src/main/java/com/sumuka/ecommerce_backend/config/CorsConfig.java
@@ -11,9 +11,12 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+            @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://omnicart-frontend-lgzumf3kn-sumukha-k-ys-projects.vercel.app")
+                        .allowedOrigins(
+                            "https://omnicart-frontend-lgzumf3kn-sumukha-k-ys-projects.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
