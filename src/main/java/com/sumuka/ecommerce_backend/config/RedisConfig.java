553d01aba173688c0c1c8bfd5518b15147d26b91
Sumukha K Y <107631405+Sumukha6357@users.noreply.github.com>
2025-07-26 11:07:41 +0530

    Update RedisConfig.java

--- src/main/java/com/sumuka/ecommerce_backend/config/RedisConfig.java
+++ src/main/java/com/sumuka/ecommerce_backend/config/RedisConfig.java
@@ -3,25 +3,41 @@ package com.sumuka.ecommerce_backend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
+    @Value("${redis.url}")
+    private String redisUrl;
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+    public LettuceConnectionFactory redisConnectionFactory() throws URISyntaxException {
+        URI redisUri = new URI(redisUrl);
+
+        String host = redisUri.getHost();
+        int port = redisUri.getPort();
+        String userInfo = redisUri.getUserInfo();
+        String password = userInfo != null && userInfo.contains(":") ? userInfo.split(":", 2)[1] : null;
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        if (password != null) {
+            config.setPassword(RedisPassword.of(password));
+        }
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate() throws URISyntaxException {
         RedisTemplate<String, Object> redis = new RedisTemplate<>();
         redis.setConnectionFactory(redisConnectionFactory());
         return redis;
